d4f46d1d7243d186cd4a3fb01bbf1e224328f649
Sadjow <sadjow@gmail.com>
2013-01-18 18:20:12 -0300

    Changing cache dir

--- albums/app/uploaders/albums/image_uploader.rb
+++ albums/app/uploaders/albums/image_uploader.rb
@@ -21,6 +21,10 @@ class Albums::ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:

01e3d03cd0d8fb3e9f94d01f528db0f181063b53
Sadjow <sadjow@gmail.com>
2013-01-18 17:03:22 -0300

    Config carrierwave

--- albums/config/initializers/carrierwave.rb
+++ albums/config/initializers/carrierwave.rb
@@ -5,4 +5,5 @@ CarrierWave.configure do |config|
   config.storage              = :grid_fs
   config.grid_fs_access_url   = "/files"
   config.cache_dir = "uploads"
+  config.root = Rails.root.join('tmp')
 end

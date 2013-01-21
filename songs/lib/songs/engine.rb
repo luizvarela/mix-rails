952160fc49c2f2a8ca0197d4f66b21a554360ca5
Sadjow <sadjow@gmail.com>
2013-01-21 10:22:56 -0300

    Menu songs added

--- songs/lib/songs/engine.rb
+++ songs/lib/songs/engine.rb
@@ -1,4 +1,19 @@
 module Songs
   class Engine < ::Rails::Engine
+    
+    def navigation
+      if defined? Admix
+        Admix::Navigation::NavBar.post_menu do
+          Admix::Navigation::NavBar.find(:content) do |menu|
+            menu.submenu do |submenu|
+              submenu.key   = :songs
+              submenu.title = I18n.t 'songs.songs'
+              submenu.url   = 'admix_songs_url'
+            end
+          end
+        end
+      end
+    end
+
   end
 end

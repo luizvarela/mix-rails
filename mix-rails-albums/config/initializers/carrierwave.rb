02005d7766ff8c60aa2a54469026d95f63e342b1
Sadjow <sadjow@gmail.com>
2013-01-22 14:37:30 -0300

    Changing default extension for songs and carrierwave upload cache config

--- mix-rails-albums/config/initializers/carrierwave.rb
+++ mix-rails-albums/config/initializers/carrierwave.rb
@@ -5,5 +5,5 @@ CarrierWave.configure do |config|
   config.storage              = :grid_fs
   config.grid_fs_access_url   = "/files"
   config.cache_dir = "uploads"
-  config.root = Rails.root.join('tmp')
+  config.root = Rails.root.join('public')
 end

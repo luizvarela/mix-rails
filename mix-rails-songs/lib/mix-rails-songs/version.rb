d650410a1aea6f241c0446624b5af2922477d908
Sadjow <sadjow@gmail.com>
2013-01-22 11:41:41 -0300

    Preparing for 0.10.2 release

--- mix-rails-songs/lib/mix-rails-songs/version.rb
+++ mix-rails-songs/lib/mix-rails-songs/version.rb
@@ -2,7 +2,7 @@ module MixRailsSongs
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 10
-    TINY  = 1
+    TINY  = 2
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')

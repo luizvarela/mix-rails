f3d2bc8a806b26fefd2b302862f98476feede1ba
Sadjow <sadjow@gmail.com>
2013-01-22 11:37:28 -0300

    Preparing for 0.10.1 release

--- mix-rails-vouchers/lib/mix-rails-vouchers/version.rb
+++ mix-rails-vouchers/lib/mix-rails-vouchers/version.rb
@@ -2,7 +2,7 @@ module MixRailsVouchers
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 10
-    TINY  = 0
+    TINY  = 1
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')

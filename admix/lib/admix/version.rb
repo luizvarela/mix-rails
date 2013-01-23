f51398876efe4f4765559617af1f203827897384
Sadjow <sadjow@gmail.com>
2013-01-23 14:41:20 -0300

    Preparing for 0.12.1 release

--- admix/lib/admix/version.rb
+++ admix/lib/admix/version.rb
@@ -2,7 +2,7 @@ module Admix
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 12
-    TINY  = 0
+    TINY  = 1
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
--- mix-rails-songs/lib/mix-rails-songs/version.rb
+++ mix-rails-songs/lib/mix-rails-songs/version.rb
@@ -2,7 +2,7 @@ module MixRailsSongs
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 12
-    TINY  = 0
+    TINY  = 1
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')

393607fa166f1cc9ffffab8d3b46b70d7cf2423f
Sadjow Leão <sadjow@gmail.com>
2013-01-29 00:12:04 -0300

    system

--- mix-rails-albums/config/initializers/carrierwave.rb
+++ mix-rails-albums/config/initializers/carrierwave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
 
   config.storage              = :grid_fs
   config.grid_fs_access_url   = "/files"
-  config.cache_dir = 'system/uploads'
+  config.cache_dir = "system"
   config.root = Rails.root.join('public')
 end

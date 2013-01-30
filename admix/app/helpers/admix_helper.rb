43b7cbff4d9304c6a2248a1d2dcc54fe0055af55
Sadjow Leão <sadjow@gmail.com>
2013-01-30 20:20:15 -0300

    Admix title

--- admix/app/helpers/admix_helper.rb
+++ admix/app/helpers/admix_helper.rb
@@ -1,5 +1,11 @@
 module AdmixHelper
   
+  include CoreHelper
+
+  def app_title
+    Setting.app_title
+  end
+
   def input_label(model_name, input_name)
     I18n.t("#{model_name}.#{input_name}", scope: :"simple_form.labels", default: [:"defaults.#{input_name}"])
   end

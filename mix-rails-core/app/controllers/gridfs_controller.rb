87e858e3864246ab30ace7ddc29deab999f4c514
Sadjow <sadjow@gmail.com>
2013-01-30 17:47:09 -0300

    Fix begin recue from gridfs controlelr

--- mix-rails-core/app/controllers/gridfs_controller.rb
+++ mix-rails-core/app/controllers/gridfs_controller.rb
@@ -4,19 +4,19 @@ require 'mix-rails-core/gridfs'
 class GridfsController < ActionController::Metal
   def serve
       
-      
+    file_dir =  File.expand_path '../', "#{Rails.root.join('public')}#{env['PATH_INFO']}"
+    Dir.mkdir file_dir unless Dir.exists?(file_dir)
+    gridfs_file = MixRailsCore::Gridfs::read_file(env["PATH_INFO"])
+    tmp_file_name = "#{Rails.root.join('public')}#{env['PATH_INFO']}"
+    file_content = gridfs_file.read
+    File.open(tmp_file_name, 'wb') do |f|
+      f.write(file_content)
+      f.close
+    end
 
     begin
       
-      file_dir =  File.expand_path '../', "#{Rails.root.join('public')}#{env['PATH_INFO']}"
-      Dir.mkdir file_dir unless Dir.exists?(file_dir)
-      gridfs_file = MixRailsCore::Gridfs::read_file(env["PATH_INFO"])
-      tmp_file_name = "#{Rails.root.join('public')}#{env['PATH_INFO']}"
-      file_content = gridfs_file.read
-      File.open(tmp_file_name, 'wb') do |f|
-        f.write(file_content)
-        f.close
-      end
+      
 
       self.response_body = file_content
 
